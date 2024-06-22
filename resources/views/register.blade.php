28c0cad1f6324b577af48bd0c915552c9553161f
Jose Rojas <josea.rcampero@gmail.com>
2024-06-21 23:55:36 -0400

    MUCHAS VAINAS

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>Registro de Usuario</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- Bootstrap CSS v5.2.1 -->
+    <!-- Bootstrap CSS v5.3.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
@@ -16,122 +16,106 @@
 
     <!-- Section: Design Block -->
     <section class="text-center text-lg-start">
-        <style>
-        .cascading-right {
-            margin-right: -50px;
-        }
-
-        @media (max-width: 991.98px) {
-            .cascading-right {
-                margin-right: 0;
-            }
-        }
-        </style>
 
         <!-- Jumbotron -->
         <div class="container py-4">
             <div class="row g-0 align-items-center">
                 <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="card cascading-right bg-body-tertiary" style="
-            backdrop-filter: blur(30px);
-            ">
-                        <div class="card-body p-5 shadow-5 text-center">
-                            <h2 class="fw-bold mb-5">Registrese Ahora</h2>
+                    <div class="card bg-body-tertiary">
+                        <div class="card-body p-5 shadow-sm text-center">
+                            <h2 class="fw-bold mb-5">Regístrese Ahora</h2>
 
-                            <form method='post' action='register' class="row g-3 needs-validation" novalidate>
-                                @csrf
+                            <form method="POST" action="{{ route('register') }}" class="row g-3">
+                            @csrf
                                 <!-- 2 column grid layout with text inputs for the first and last names -->
                                 <div class="row">
                                     <div class="col-md-6 mb-4">
-                                        <div data-mdb-input-init class="form-outline">
-                                            <label class="form-label" for="form3Example1">Nombres</label>
+                                        <div class="form-outline">
+                                            <label class="form-label" for="name">Nombres</label>
                                             <input type="text" id="name" name="name"
                                                 class="form-control @error('name') is-invalid @enderror"
-                                                value="{{old('name')}}" />
+                                                value="{{ old('name') }}" />
                                             @error('name')
-                                            <span class="invalid-feedback">
-                                                <strong>{{$message}}</strong>
-                                            </span>
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
                                             @enderror
                                         </div>
                                     </div>
                                     <div class="col-md-6 mb-4">
-                                        <div data-mdb-input-init class="form-outline">
-                                            <label class="form-label" for="form3Example2">Apellidos</label>
+                                        <div class="form-outline">
+                                            <label class="form-label" for="last_name">Apellidos</label>
                                             <input type="text" id="last_name" name="last_name"
                                                 class="form-control @error('last_name') is-invalid @enderror"
-                                                value="{{old('last_name')}}" />
+                                                value="{{ old('last_name') }}" />
                                             @error('last_name')
-                                            <span class="invalid-feedback">
-                                                <strong>{{$message}}</strong>
-                                            </span>
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
                                             @enderror
                                         </div>
                                     </div>
                                 </div>
 
                                 <!-- User input -->
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form3Example3">Usuario</label>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="user">Usuario</label>
                                     <input type="text" id="user" name="user"
                                         class="form-control @error('user') is-invalid @enderror"
-                                        value="{{old('user')}}" />
+                                        value="{{ old('user') }}" />
                                     @error('user')
-                                    <span class="invalid-feedback">
-                                        <strong>{{$message}}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
 
                                 <!-- Email input -->
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form3Example3">Correo Electrónico</label>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="email">Correo Electrónico</label>
                                     <input type="email" id="email" name="email"
                                         class="form-control @error('email') is-invalid @enderror"
-                                        value="{{old('email')}}" />
+                                        value="{{ old('email') }}" />
                                     @error('email')
-                                    <span class="invalid-feedback">
-                                        <strong>{{$message}}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
 
                                 <!-- Password input -->
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form3Example4">Contraseña</label>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="password">Contraseña</label>
                                     <input type="password" id="password" name="password"
-                                        class="form-control @error('password') is-invalid @enderror"
-                                        value="{{old('password')}}" />
+                                        class="form-control @error('password') is-invalid @enderror" />
                                     @error('password')
-                                    <span class="invalid-feedback">
-                                        <strong>{{$message}}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
 
                                 <!-- Confirm Password input -->
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form3Example4">Confirmar Contraseña</label>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="password_confirmation">Confirmar Contraseña</label>
                                     <input type="password" id="password_confirmation" name="password_confirmation"
-                                        class="form-control @error('password_confirmation') is-invalid @enderror"
-                                        value="{{old('password_confirmation')}}" />
+                                        class="form-control @error('password_confirmation') is-invalid @enderror" />
                                     @error('password_confirmation')
-                                    <span class="invalid-feedback">
-                                        <strong>{{$message}}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
 
                                 <!-- Submit button -->
-                                <button type="submit" data-mdb-button-init data-mdb-ripple-init value="Enviar"
-                                    class="btn btn-primary btn-block mb-4">
+                                <button type="submit" class="btn btn-primary btn-block mb-4">
                                     Registrarse
                                 </button>
 
-                                <!--Submit login-->
-                                <div class="d-flex aling-items-center justify-content-center pb-4">
+                                <!-- Submit login -->
+                                <div class="d-flex align-items-center justify-content-center pb-4">
                                     <p class="mb-0 me-2">¿Ya tienes cuenta?</p>
-                                    <a href="{{route('login')}}" class="btn btn-outline-danger">Inicia Sesión</a>
+                                    <a href="{{ route('login') }}" class="btn btn-outline-danger">Inicia Sesión</a>
                                 </div>
                             </form>
                         </div>
@@ -140,7 +124,7 @@
 
                 <div class="col-lg-6 mb-5 mb-lg-0">
                     <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-                        class="w-100 rounded-4 shadow-4" alt="" />
+                        class="w-100 rounded-4 shadow-sm" alt="" />
                 </div>
             </div>
         </div>
